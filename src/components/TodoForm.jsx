41a694c61350ff9ee752d270a9cf5f6cc9ad6a97
Leo10Demigod <leo10demigod@gmail.com>
2024-04-09 15:55:02 +0530

    made some changes

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,13 @@
 import React , {useState} from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
 import { database } from '../Appwrite/appwriteConfig';
 import {v4 as uuidv4} from 'uuid'
 const TodoForm = () => {
 
 const [todo , setTodo] = useState("")
+const [date , setDate] = useState("")
+const [subtask , setSubTask] = useState("")
+const [open , setOpen] = useState(false)
 
   const handleSubmit = (e) =>{
    e.preventDefault();
@@ -24,16 +28,36 @@ const [todo , setTodo] = useState("")
 
   return (
     <div className=' mx-auto w-[100%]  md:w-[75%] items-center  p-4 py-10'>
-      <form action="" onSubmit={handleSubmit} className='flex'>
+      <form action=""  className='flex'>
       <input
-      onChange={(e)=>{
-        setTodo(e.target.value)
-      }}
+      onClick={()=>{setOpen(!open)}}
       type="text" placeholder='Enter todo here' className='bg-transparent px-2 py-3  outline-none border-b-2 border-red-700 font-bold w-[85%] text'/>
       <button type='submit' className='card text-white  text-[0.8rem] lg:text-[1rem] font-bold rounded-md p-1 md:p-2 md:ml-[1rem]'>Add Todo</button>
       </form>
+      <div className={open?'card2 fixed opacity-100 bg-red-950/80 z-10 h-screen w-full top-0 left-0':'absolute opacity-0'}>
+<div className='card w-[97%] md:w-[50%] lg:w-[40%] mx-auto mt-[2%] p-3 rounded-[1rem]'>
+    <div className='flex items-center justify-between'>
+      <h1 className='font-bold '>Create a Todo</h1>
+      <p className='cursor-pointer' onClick={()=>setOpen(!open)}><AiOutlineClose/></p>
+    </div>
+    <div className='py-3'>
+      <textarea 
+      onSubmit={handleSubmit}
+      onChange={(e)=>{
+        setTodo(e.target.value)
+      }} className='w-full outline-none bg-transparent' placeholder='What do you want to do?' name="" id="" cols="30" rows="10"></textarea>
+    </div>
+    <div className='flex items-center justify-between'>
+    <div onClick={handleSubmit} className='text-white card2  w-full text-center cursor-pointer mr-5 px-4 py-2 rounded-[1.3rem] font-bold'>
+      <button type='submit'>Post</button>
+    </div>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
 
 export default TodoForm
+
+
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,92 +1,94 @@
-import React , {useEffect , useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import { database } from '../Appwrite/appwriteConfig';
-import { FaTrash } from "react-icons/fa";
-import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
-import { RiCheckboxFill } from "react-icons/ri";
+import { FaTrash } from 'react-icons/fa';
+import { MdOutlineCheckBoxOutlineBlank } from 'react-icons/md';
+import { RiCheckboxFill } from 'react-icons/ri';
 
 function Todos() {
-  const [todos , setTodos] = useState()
-  const[loader , setLoader] = useState(false)
-
-  useEffect(()=>{
-    setLoader(true)
-    const getTodos =database.listDocuments("65c3b9197c661a56a1a9" ,"65c3b922657323e3b50d")
+  const [todos, setTodos] = useState([]);
+  const [loader, setLoader] = useState(false);
+  
+  useEffect(() => {
+    setLoader(true);
+    const getTodos = database.listDocuments('65c3b9197c661a56a1a9', '65c3b922657323e3b50d');
     getTodos.then(
-      function(response){
-        setTodos(response.documents)
-        setLoader(false)
+      function (response) {
+        setTodos(response.documents);
+        setLoader(false);
       },
-      function(error){
-        console.log(error)
+      function (error) {
+        console.log(error);
       }
-    )
-  } , [])
+    );
+  }, []);
 
   const deleteTodo = (id) => {
-    const promise =  database.deleteDocument("65c3b9197c661a56a1a9" ,"65c3b922657323e3b50d",id)
+    const promise = database.deleteDocument('65c3b9197c661a56a1a9', '65c3b922657323e3b50d', id);
     promise.then(
-     function(response){
-         console.log(response);
-         window.location.reload()
-     },
-     function(error){
-         console.log(error);
-     }
-   )
- }
- const toggleTodo = (id) => {
-  setTodos((prevTodos) =>
-    prevTodos.map((todo) => {
-      if (todo.$id === id) {
-        // Toggle the 'completed' status for the clicked todo
-        return { ...todo, completed: !todo.completed };
+      function (response) {
+        console.log(response);
+        const updatedTodos = todos.filter((todo) => todo.$id !== id);
+        setTodos(updatedTodos);
+      },
+      function (error) {
+        console.log(error);
       }
-      return todo;
-    })
-  );
-};
-
-//  const doneTodo = (id) => {
-//    setIcon(!icon , id)
-// }
-
+    );
+  };
+  
+  useEffect(() => {
+    const getTodos = database.listDocuments('65c3b9197c661a56a1a9', '65c3b922657323e3b50d');
+    getTodos.then(
+      function (response) {
+        setTodos(response.documents);
+        localStorage.setItem('todos', JSON.stringify(response.documents)); // Save todos from database to local storage
+        setLoader(false);
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
+  }, []);
+  
 
   return (
     <div className="max-w-7xl mx-auto">
-    <p className="text-xl font-bold mb-2 text-center text-white">Todo List</p>
-    {loader ? (
-      <p className='text-center text font-bold text-[1.3rem]'>Loading ...</p>
-    ) : (
-      <div>
-        {todos &&  todos.map(item => (
-              <div key={item.$id} >
-              <div className={!item.completed?"duration-250 p-4 flex items-center justify-between mt-2 medium font-bold text-[1.1rem] text-white rounded-lg mb-1 w-[100%] md:w-[72%] mx-auto":"line-through duration-250 p-4 flex items-center justify-between mt-2 cross font-bold text-[1.1rem] text-white rounded-lg mb-1 w-[100%] md:w-[72%] mx-auto"}>
-                <div>
-                  <p>{item.todo}</p>
-                </div>
-                <div className='flex items-center gap-2'>
-                  <span
-                    className="text-white cursor-pointer duration-300"
-                    onClick={() =>{toggleTodo(item.$id)}}>
-                    {item.completed ? <RiCheckboxFill size={23} /> : <MdOutlineCheckBoxOutlineBlank size={23} />}
-                  </span>
-                  <span
-                    className="text-white cursor-pointer"
-                    onClick={() => {
-                      deleteTodo(item.$id)
-                    }}
-                  >
-                    <FaTrash  />
-                  </span>
+      <p className="text-xl font-bold mb-2 text-center text-white">Todo List</p>
+      {loader ? (
+        <p className="text-center text font-bold text-[1.3rem]">Loading ...</p>
+      ) : (
+        <div>
+          {todos &&
+            todos.map((item) => (
+              <div key={item.$id}>
+                <div
+                  className={
+                    !item.completed
+                      ? 'duration-250 p-4 flex items-center justify-between mt-2 medium font-bold text-[1.1rem] text-white rounded-lg mb-1 w-[100%] md:w-[72%] mx-auto'
+                      : 'line-through duration-250 p-4 flex items-center justify-between mt-2 cross font-bold text-[1.1rem] text-white rounded-lg mb-1 w-[100%] md:w-[72%] mx-auto'
+                  }
+                >
+                  <div>
+                    <p>{item.todo}</p>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <span
+                      className="text-white cursor-pointer"
+                      onClick={() => {
+                        deleteTodo(item.$id);
+                      }}
+                    >
+                      <FaTrash />
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-            )) }
-          
-      </div>
-    )}
-  </div>
-  )
+            ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Todos
+export default Todos;
+
